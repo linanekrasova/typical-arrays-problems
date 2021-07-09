e31b7f8ee9764ddcb4d611df91b4201a0ed695c1
linanekrasova <sumanera@yandex.ru>
2021-07-09 21:24:05 +0300

    feat: implement min, max and average functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(num) {
+    if (!num || !num.length) {
+        return 0;
+    }
+    let min = num[0];
+    for (let i = 1; i < num.length; i++) {
+        if (num[i] < min) {
+            min = num[i];
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(num) {
+    if (!num || !num.length) {
+        return 0;
+    }
+    let max = num[0];
+    for (let i = 1; i < num.length; i++) {
+        if (num[i] > max) {
+            max = num[i];
+        }
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(num) {
+    if (!num || !num.length) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+        sum += num[i];
+    }
+    return sum / num.length;
+};
